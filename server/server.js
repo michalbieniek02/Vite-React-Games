5f4bb6ff807ebf6cd4966240023b9202682a2d3e
michalbieniek02 <michabieniek02@gmail.com>
2024-06-13 10:19:36 +0200

    added ngrok backend

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  cors: "https://localhost:5174/",
+  cors: "http://localhost:5174/",
 });
 
 const allUsers = {};
