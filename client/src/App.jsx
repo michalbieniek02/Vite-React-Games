19c8e5a858dbf7ab0e668289bc780d9e4d0ef212
michalbieniek02 <michabieniek02@gmail.com>
2024-06-18 14:53:02 +0200

    multiplayer tictactoe is now workng

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import Footer from './components/Footer';
-import MobileFooter from './components/MobileFooter';
-import NavBar from './components/NavBar';
-import Home from './sites/Home';
-import MemoryGame from './sites/MemoryGame';
-import TicTacToe from './sites/TicTacToe';
-import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import CreateRoom from './components/CreateRoom/CreateRoom';
+import JoinRoom from './components/JoinRoom/JoinRoom';
+import Game from './components/Game/Game';
+import Footer from './components/Footer.jsx';
+import MobileFooter from './components/MobileFooter.jsx';
+import NotFound from './components/NotFound/NotFound';
+import NavBar from './components/NavBar.jsx'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client';
+import MemoryGame from './sites/MemoryGame.jsx'
 
-const App = () => {
+const socket = io.connect('http://localhost:3000');
+// const socket = io.connect(process.env.REACT_APP_SOCKET_SERVER_URL);
+
+function App() {
+
+  // const {user} = useSelector(state => state.user)
+
   return (
-    <>
+    <div className="App">
+      <header className="App-header">
+
+    <Router>
       <NavBar/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/tic-tac-toe" element={<TicTacToe/>}/>
-        <Route path="/tic-tac-toe/:roomId" element={<TicTacToe/>}/>
+      <Route path='/' element={<Home />}></Route>
+        <Route path='/createRoom' element={<CreateRoom socket={socket}/>}></Route>
+        <Route path='/joinRoom' element={<JoinRoom socket={socket}/>}></Route>
+        <Route path='/game/:roomId' element={<Game socket={socket}/>}></Route>
         <Route path="/memory" element={<MemoryGame/>}/>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
       <MobileFooter/>
       <Footer/>
-    </>
-  )
+    </Router>
+
+
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ server/users.js
@@ -0,0 +1,145 @@
+const users = [];
+const gameDetail = [];
+
+
+function addUser(socketId, roomId) {
+  users.push({socketId, roomId});
+}
+
+function getCurrentUser(id) {
+  return users.find(user => user.id === id);
+}
+
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+
+function addRoom(room) {
+  let isRoomExsist = game.find(item => item.room === room);
+  if(!isRoomExsist) {
+    game.push({ 
+      room,
+      users:[],
+      moves: {} 
+    });
+  }
+}
+
+
+function newGame (room, userId, username) {
+  
+  let isRoomExsist = gameDetail.find(item => item.room === room);
+  if(!isRoomExsist) {
+    let newGameDetail = [];
+    newGameDetail = {room, user1:{userId , username, moves: [], winCount:0, inGame:false}, user2:{userId:0 , username:0, moves: [], winCount:0, inGame:false}};
+
+    gameDetail.push(newGameDetail);
+  }
+  else{
+    if(isRoomExsist.user2.userId === 0 && isRoomExsist.user1.userId != userId){
+      isRoomExsist.user2.userId = userId;
+      isRoomExsist.user2.username = username;
+    }
+    else{
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function getGameDetail(room) {
+  return gameDetail.find(item => item.room === room)
+}
+
+function addMove(room, userId, move) {
+  let gameDetail = getGameDetail(room);
+  if(gameDetail.users[0]==socket.id){
+
+  }
+  gameDetail.moves.push(move);
+}
+
+const winPatterns = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+
+function CheckWin(room, userId) {
+  let gameDetail = getGameDetail(room);
+
+  let user;
+  let curr_user_moves;
+  let winCount;
+  if(gameDetail.user1.userId==userId){
+    user = 1;
+    curr_user_moves = gameDetail.user1.moves;
+  }
+  else{
+    user = 2;
+    curr_user_moves = gameDetail.user2.moves;
+  }
+
+  let pattern;
+  let isWin;
+  for(let i=0; i<winPatterns.length; i++){
+    let win_pattern = winPatterns[i];
+    isWin = true;
+    for(let j=0; j<win_pattern.length; j++){
+      if(!curr_user_moves.includes(win_pattern[j])){
+        isWin = false;
+      }
+    }
+    if(isWin){
+      pattern = i;
+      if(user===1){
+        gameDetail.user1.winCount = gameDetail.user1.winCount +1;
+        winCount = gameDetail.user1.winCount;
+      }
+      else{
+        gameDetail.user2.winCount = gameDetail.user2.winCount +1;
+        winCount = gameDetail.user1.winCount;
+      }
+      break;
+    }
+  }
+
+  return {isWin,winCount, pattern};
+}
+
+function removeRoom(room) {
+  let index = gameDetail.findIndex(item => item.room === room);
+  if(index !== -1){
+    return gameDetail.splice(index, 1)[0];
+  }
+}
+
+
+function userLeft(socketId) {
+  if(!users.find(user=>user.socketId === socketId)){
+    return;
+  }
+  let roomId = users.find(user => user.socketId === socketId).roomId;
+  let index = users.findIndex(user => user.socketId === socketId);
+  if(index !== -1){
+    users.splice(index, 1)[0];
+  }
+  removeRoom(roomId);
+  return roomId;
+}
+
+
+module.exports = {
+  getCurrentUser,
+  userLeave,
+  addRoom,
+  addMove,
+  getGameDetail,
+  newGame,
+  CheckWin,
+  removeRoom,
+  addUser,
+  userLeft
+};
