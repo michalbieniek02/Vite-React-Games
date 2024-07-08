e2bd7d283b22f3c11a39e8db3952d51c46360716
Michał Bieniek <michalbieniek02@gmail.com>
2024-07-08 14:07:32 +0200

    edited vite config for docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRefresh()],
   server: {
     port: 5173
   },
-})
+});
